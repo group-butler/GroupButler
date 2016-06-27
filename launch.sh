529ac6c22e8f0c83b391ed2c993e2cb7fe316bed
jarriztg <jarriztg@users.noreply.github.com>
2016-06-26 20:18:18 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-while true; do
-	lua bot.lua
-	sleep 4s
-done
+if [ "$1" = "" ]; then
+	while true; do
+		lua bot.lua
+		sleep 4s
+	done
+fi
+
+
+if [ "$1" = "tmux" ]; then
+  clear
+  tmux kill-session -t tmuxsession 
+  tmux new-session -d -s tmuxsession "lua bot.lua" && echo -e '\e[0;32mBot started with tmux.\e[0m'
+  echo "Nombre de tmux con nombre running" || echo -e '\e[0;31mError.\e[0m'
+
+fi
+
+if [ "$1" = "attach" ]; then
+  clear
+  tmux attach -t tmuxsession
+fi
+
+if [ "$1" = "kill" ]; then
+  clear
+  tmux kill-session -t tmuxsession
+fi
