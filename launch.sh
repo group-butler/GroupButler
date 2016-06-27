a057da36d77ef3769bba28d68eb4e13e59e0f6f0
jarriztg <jarriztg@users.noreply.github.com>
2016-06-26 20:28:40 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -9,8 +9,8 @@ fi
 
 
 if [ "$1" = "tmux" ]; then
-  clear
   tmux kill-session -t tmuxsession 
+  clear
   tmux new-session -d -s tmuxsession "lua bot.lua" && echo -e '\e[0;32mBot started sucessfully.\e[0m'
   echo "Tmux session with name tmuxsession" || echo -e '\e[0;31mError.\e[0m'
 
