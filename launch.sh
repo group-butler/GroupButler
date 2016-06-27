644e4d7857002e3336bd98a31280412c6e2d5339
jarriztg <jarriztg@users.noreply.github.com>
2016-06-26 20:53:30 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Tmux session name
+NAME=GroupButler
+
 if [ "$1" = "" ]; then
 	while true; do
 		lua bot.lua
@@ -9,19 +12,19 @@ fi
 
 
 if [ "$1" = "tmux" ]; then
-  tmux kill-session -t tmuxsession 
+  tmux kill-session -t $NAME 
   clear
-  tmux new-session -d -s tmuxsession "lua bot.lua" && echo -e '\e[0;32mBot started sucessfully.\e[0m'
-  echo "Tmux session with name tmuxsession" || echo -e '\e[0;31mError.\e[0m'
+  tmux new-session -d -s $NAME "lua bot.lua" && echo -e '\e[0;32mBot started sucessfully.\e[0m'
+  echo "Tmux session with name $NAME" || echo -e '\e[0;31mError.\e[0m'
 
 fi
 
 if [ "$1" = "attach" ]; then
   clear
-  tmux attach -t tmuxsession
+  tmux attach -t $NAME
 fi
 
 if [ "$1" = "kill" ]; then
   clear
-  tmux kill-session -t tmuxsession
+  tmux kill-session -t $NAME
 fi
