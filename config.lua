299eb52def979b5ea99a1eef4538674e70959cc4
mediasopel1381223 <sopelbot@gmail.com>
2016-12-26 23:33:54 +0330

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,12 +1,12 @@
 return {
-	bot_api_key = "", --Please add your bot api key here!
+	bot_api_key = "303291904:AAEAKSLiTPqp75-mcGGdeNumhECYIbYvj1c", --Please add your bot api key here!
 	cmd = '^[/!#]',
 	allowed_updates = {"message", "edited_message", "callback_query"},
 	db = 2, --default redis db: 0
 	superadmins = {23646077, 278941742},
 	log = {
 		chat = -1001089188566, --Your log chat, where your bot must be added!
-		admin = 23646077, --The admin.
+		admin = 302539892, --The admin.
 		stats = nil
 	},
 	human_readable_version = '4.2.0',
