92783bc985e524ad97814f7ab7ad95cc28c76530
Diego Barreiro <diego.barreiro.perez@gmail.com>
2017-03-05 15:39:19 +0100

    Update

--- plugins/apod.lua
+++ plugins/apod.lua
@@ -10,23 +10,21 @@ function plugin.onTextMessage(msg, blocks)
 	if blocks[1] == 'apod' then
 		local base_url = "http://barreeeiroo.ga/BarrePolice/apod/?key="..config.apod_api_key
 		if not blocks[2] then
-			local message = """*Command methods*\n\n- /apod `image` - _Sends the NASA Image of the day_\n- /apod `hd` - Sends the NASA Image of the day in HD quality_\n- /apod `data` - Sends the data of the NASA Image of the day_"""
+			local message = "*Avaliable Commands:*\n\n- /apod `image` - _Sends the NASA Image of the day_\n- /apod `hd` - Sends the NASA Image of the day in HD quality_\n- /apod `data` - Sends the data of the NASA Image of the day_"
 			api.sendReply(msg, message, true, reply_markup)
 		else
 			if blocks[2] == 'image' then
 				local url = base_url .. "&image"
 				local output, res = HTTP.request(url)
 		    if not output or res ~= 200 or output:len() == 0 then
-		        url = url = base_url .. "&image"
 		        output, res = HTTP.request(url)
 		    end
 				local message = "<a href='"..output.."'></a>'"
 				api.sendReply(msg, message, true, html, reply_markup, link_preview)
 			elseif blocks[2] == 'hd' then
-				local url = base_url .. "&image"
+				local url = base_url .. "&hd"
 				local output, res = HTTP.request(url)
 		    if not output or res ~= 200 or output:len() == 0 then
-		        url = url = base_url .. "&hd"
 		        output, res = HTTP.request(url)
 		    end
 				local message = "<a href='"..output.."'></a>'"
@@ -35,7 +33,6 @@ function plugin.onTextMessage(msg, blocks)
 				local url = base_url .. "&data"
 				local output, res = HTTP.request(url)
 		    if not output or res ~= 200 or output:len() == 0 then
-		        url = url = base_url .. "&image"
 		        output, res = HTTP.request(url)
 		    end
 				api.sendReply(msg, output, true, html, reply_markup)
