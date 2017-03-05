66edce417cd46d43cd079f49fbdde6db6bb28dc1
Diego Barreiro <diego.barreiro.perez@gmail.com>
2017-03-05 15:47:40 +0100

    Update

--- plugins/apod.lua
+++ plugins/apod.lua
@@ -10,7 +10,7 @@ function plugin.onTextMessage(msg, blocks)
 	if blocks[1] == 'apod' then
 		local base_url = "http://barreeeiroo.ga/BarrePolice/apod/?key="..config.apod_api_key
 		if not blocks[2] then
-			local message = "*Avaliable Commands:*\n\n- /apod `image` - _Sends the NASA Image of the day_\n- /apod `hd` - Sends the NASA Image of the day in HD quality_\n- /apod `data` - Sends the data of the NASA Image of the day_"
+			local message = "*Avaliable Commands:*\n\n- /apod `image` - _Sends the NASA Image of the day_\n- /apod `hd` - _Sends the NASA Image of the day in HD quality_\n- /apod `data` - Sends the data of the NASA Image of the day_"
 			api.sendReply(msg, message, true, reply_markup)
 		else
 			if blocks[2] == 'image' then
@@ -20,7 +20,7 @@ function plugin.onTextMessage(msg, blocks)
 		        output, res = HTTP.request(url)
 		    end
 				local message = "<a href='"..output.."'></a>'"
-				api.sendReply(msg, message, true, html, reply_markup, link_preview)
+				api.sendReply(msg, message, true, html, link_preview)
 			elseif blocks[2] == 'hd' then
 				local url = base_url .. "&hd"
 				local output, res = HTTP.request(url)
@@ -28,14 +28,14 @@ function plugin.onTextMessage(msg, blocks)
 		        output, res = HTTP.request(url)
 		    end
 				local message = "<a href='"..output.."'></a>'"
-				api.sendReply(msg, message, true, html, reply_markup, link_preview)
+				api.sendReply(msg, message, true, html, link_preview)
 			elseif blocks[2] == 'data' then
 				local url = base_url .. "&data"
 				local output, res = HTTP.request(url)
 		    if not output or res ~= 200 or output:len() == 0 then
 		        output, res = HTTP.request(url)
 		    end
-				api.sendReply(msg, output, true, html, reply_markup)
+				api.sendReply(msg, output, true, reply_markup)
 			else
 				message = "Unknown request"
 				api.sendReply(msg, message, true, reply_markup)
