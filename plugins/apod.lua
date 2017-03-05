41305f9eaf57d677f3794d2a3e71a424c6a823e7
Diego Barreiro <diego.barreiro.perez@gmail.com>
2017-03-05 15:26:56 +0100

    Update

--- plugins/apod.lua
+++ plugins/apod.lua
@@ -10,11 +10,11 @@ function plugin.onTextMessage(msg, blocks)
 	if blocks[1] == 'apod' then
 		local base_url = "http://barreeeiroo.ga/BarrePolice/apod/?key="..config.apod_api_key
 		if not blocks[2] then
-			message = _([[
-			*Command methods
-- /apod `image` - _Sends the NASA Image of the day_
-- /apod `hd` - Sends the NASA Image of the day in HD quality_
-- /apod `data` - Sends the data of the NASA Image of the day_]])
+			message = "
+*Command methods*\n
+- /apod `image` - _Sends the NASA Image of the day_\n
+- /apod `hd` - Sends the NASA Image of the day in HD quality_\n
+- /apod `data` - Sends the data of the NASA Image of the day_"
 			api.sendReply(msg, message, true, reply_markup)
 		else
 			if blocks[2] == 'image' then
