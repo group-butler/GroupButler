f6d13bab08e7a0c70e3670ca3a2a1ae408700a24
Yan Minari <yangm97@gmail.com>
2017-04-21 23:54:39 -0300

    Revert "Remove unnecessary plugin"
    This reverts commit 690e46d2425499c42e8a295601220528be0f0e01.

--- config.lua
+++ config.lua
@@ -46,6 +46,7 @@ return {
 		'onmessage', --THIS MUST BE THE FIRST: IF AN USER IS FLOODING/IS BLOCKED, THE BOT WON'T GO THROUGH PLUGINS
 		'antispam', --SAME OF onmessage.lua
 		--'realms', --must stay here
+		'backup',
 		'banhammer',
 		'block',
 		'configure',
