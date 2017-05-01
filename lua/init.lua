771d87108af96aae6cbf9f8557c90895d4194e82
Yan Minari <yangm97@gmail.com>
2017-05-01 03:02:11 -0300

    Use single quotes
    Signed-off-by: Yan Minari <yangm97@gmail.com>

--- lua/init.lua
+++ lua/init.lua
@@ -1,7 +1,7 @@
 redis = require 'resty.redis' -- Load redis client
 pgmoon = require 'pgmoon' -- Load postgres client
 json = require 'cjson' -- Load json library
-http = require "resty.http" -- Load resty http library
+http = require 'resty.http' -- Load resty http library
 config = require 'config' -- Load configuration file
 i18n = require 'i18n' -- Load localization library
 api = require 'methods' -- Load Telegram API
