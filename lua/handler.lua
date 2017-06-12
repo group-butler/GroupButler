edf4957be108072c9ac0e22bdb2d9b0e7feee0e7
Yan Minari <yangm97@gmail.com>
2017-06-12 11:37:44 -0300

    Handle chat quits properly
    Signed-off-by: Yan Minari <yangm97@gmail.com>

--- lua/handler.lua
+++ lua/handler.lua
@@ -43,11 +43,7 @@ local function extract_usernames(msg)
 		db.setvcu(msg.chat.id, msg.new_chat_member.id, 'membership', 'true') -- Enable membership of/create entry for this chat user
 	end
 	if msg.left_chat_member then
-		if msg.left_chat_member.username then
-			username = msg.left_chat_member.username:lower()
-			userid = msg.left_chat_member.id
-		end
-		db.setvcu(msg.chat.id, msg.new_chat_member.id, 'membership', 'false') -- Disable the membership of this chat user
+		db.setvcu(msg.chat.id, msg.left_chat_member.id, 'membership', 'false') -- Disable the membership of this chat user
 	end
 	if msg.reply_to_message then
 		extract_usernames(msg.reply_to_message)
