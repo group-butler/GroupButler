651a948072558bb357271b3f9b14ce666b462de5
Yan Minari <yangm97@gmail.com>
2017-06-15 22:41:24 -0300

    fix admin.lua identation
    Signed-off-by: Yan Minari <yangm97@gmail.com>

--- lua/plugins/admin.lua
+++ lua/plugins/admin.lua
@@ -67,12 +67,12 @@ end
 
 local function match_pattern(pattern, text)
   if text then
-  	text = text:gsub('@'..bot:get('username'), '')
-    local matches = {}
-    matches = { string.match(text, pattern) }
-    if next(matches) then
-    	return matches
-    end
+		text = text:gsub('@'..bot:get('username'), '')
+		local matches = {}
+		matches = { string.match(text, pattern) }
+		if next(matches) then
+			return matches
+		end
   end
 end
 
@@ -96,33 +96,33 @@ function plugin.onTextMessage(msg, blocks)
 		end
 		api.sendMessage(msg.from.id, text)
 	end
-    if blocks[1] == 'stats' then
-    	local text = '#stats `['..u.get_date()..']`:\n'
-        local hash = 'bot:general'
-	    local names = db:hkeys(hash)
-	    local num = db:hvals(hash)
-	    for i=1, #names do
-	        text = text..'- *'..names[i]..'*: `'..num[i]..'`\n'
-	    end
-	    text = text..'- *uptime*: `from '..(os.date("%c", start_timestamp))..' (GMT+2)`\n'
-	    text = text..'- *last hour msgs*: `'..last.h..'`\n'
-	    text = text..'   • *average msgs/minute*: `'..round((last.h/60), 3)..'`\n'
-	    text = text..'   • *average msgs/second*: `'..round((last.h/(60*60)), 3)..'`\n'
+	if blocks[1] == 'stats' then
+		local text = '#stats `['..u.get_date()..']`:\n'
+		local hash = 'bot:general'
+		local names = db:hkeys(hash)
+		local num = db:hvals(hash)
+		for i=1, #names do
+			text = text..'- *'..names[i]..'*: `'..num[i]..'`\n'
+		end
+		text = text..'- *uptime*: `from '..(os.date("%c", start_timestamp))..' (GMT+2)`\n'
+		text = text..'- *last hour msgs*: `'..last.h..'`\n'
+		text = text..'   • *average msgs/minute*: `'..round((last.h/60), 3)..'`\n'
+		text = text..'   • *average msgs/second*: `'..round((last.h/(60*60)), 3)..'`\n'
 
-	    --db info
-	    text = text.. '\n*DB stats*\n'
+		--db info
+		text = text.. '\n*DB stats*\n'
 		local dbinfo = db:info()
-	    text = text..'- *uptime days*: `'..dbinfo.server.uptime_in_days..'('..dbinfo.server.uptime_in_seconds..' seconds)`\n'
-	    text = text..'- *keyspace*:\n'
-	    for dbase,info in pairs(dbinfo.keyspace) do
-	    	for real,num in pairs(info) do
-	    		local keys = real:match('keys=(%d+),.*')
-	    		if keys then
-	    			text = text..'  '..dbase..': `'..keys..'`\n'
-	    		end
-	    	end
-    	end
-    	text = text..'- *ops/sec*: `'..dbinfo.stats.instantaneous_ops_per_sec..'`\n'
+		text = text..'- *uptime days*: `'..dbinfo.server.uptime_in_days..'('..dbinfo.server.uptime_in_seconds..' seconds)`\n'
+		text = text..'- *keyspace*:\n'
+		for dbase,info in pairs(dbinfo.keyspace) do
+			for real,num in pairs(info) do
+				local keys = real:match('keys=(%d+),.*')
+				if keys then
+					text = text..'  '..dbase..': `'..keys..'`\n'
+				end
+			end
+		end
+		text = text..'- *ops/sec*: `'..dbinfo.stats.instantaneous_ops_per_sec..'`\n'
 
 		api.sendMessage(msg.chat.id, text, true)
 	end
@@ -192,15 +192,15 @@ function plugin.onTextMessage(msg, blocks)
 		end
 		return msg.text:gsub('###forward:', '')
 	end
-    if blocks[1] == 'api errors' then
-    	local errors = db:hkeys('bot:errors')
-    	local times = db:hvals('bot:errors')
-    	local text = 'Api errors:\n'
-    	for i=1,#errors do
-    		text = text..errors[i]..': '..times[i]..'\n'
-    	end
-    	api.sendMessage(msg.from.id, text)
-    end
+	if blocks[1] == 'api errors' then
+		local errors = db:hkeys('bot:errors')
+		local times = db:hvals('bot:errors')
+		local text = 'Api errors:\n'
+		for i=1,#errors do
+			text = text..errors[i]..': '..times[i]..'\n'
+		end
+		api.sendMessage(msg.from.id, text)
+	end
 	if blocks[1] == 'sendfile' then
 		local path = blocks[2]
 		api.sendDocument(msg.from.id, path)
