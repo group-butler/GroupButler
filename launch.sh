af2a3f8c00d07861b56852c5119c9f8a61af6a9f
Yan Minari <yangm97@gmail.com>
2017-06-26 04:01:26 -0300

    Move code to lua subfolder
    Signed-off-by: Yan Minari <yangm97@gmail.com>

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 make_template() {
-	find . -name "*.lua" | sort |
+	find . -name "lua/*.lua" | sort |
 		xgettext --from-code=utf-8 \
 			--add-comments=TRANSLATORS \
 			--package-name=GroupButler \
@@ -14,7 +14,7 @@ make_template() {
 
 case $1 in bot | "")
 	while true; do
-		lua bot.lua
+		lua lua/bot.lua
 		sleep 10
 	done
 
