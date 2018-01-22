3262761abe78e5bc4d5c28ab885b1d6c7398b411
igunublue <IGUNUBLUE@users.noreply.github.com>
2018-01-22 12:32:17 -0500

    Update config.lua

--- lua/config.lua
+++ lua/config.lua
@@ -48,7 +48,7 @@ local _M =
 	},
 
 	-- Aesthetic
-	lang = os.getenv('DEFAULT_LANG') or 'es',
+	lang = os.getenv('DEFAULT_LANG') or 'en',
 	human_readable_version = os.getenv('VERSION') or 'unknown',
 	channel = os.getenv('CHANNEL') or '@groupbutler_beta',
 	source_code = os.getenv('SOURCE') or 'https://github.com/RememberTheAir/GroupButler/tree/beta',
