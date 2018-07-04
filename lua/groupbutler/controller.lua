74c5a90de3a4598fca0c0ab3f12e2aa2c42cacf3
Yan Minari <yangm97@gmail.com>
2018-07-04 19:21:50 -0300

    Hide token from logs, ignore troublesome updates
    Signed-off-by: Yan Minari <yangm97@gmail.com>

--- lua/groupbutler/controller.lua
+++ lua/groupbutler/controller.lua
@@ -3,28 +3,29 @@ local _M = {}
 local json = require "cjson"
 local main = require "groupbutler.main"
 
+local function process_update(_, u)
+	main.parseMessageFunction(u)
+end
+
 function _M.run()
-	-- Decode the update
+	local ok, update
 	ngx.req.read_body()
-	local update = ngx.req.get_body_data()
+	update = ngx.req.get_body_data()
 	ngx.log(ngx.DEBUG, "Incoming update:", update)
 
-	local ok, res = pcall(json.decode, update)
+	ok, update = pcall(json.decode, update)
 	if not ok then
 		ngx.status = ngx.HTTP_BAD_REQUEST
 		return ngx.exit(ngx.HTTP_BAD_REQUEST)
 	end
 
-	update = res
+	-- Process update on another thread. Returns HTTP_OK instantly, regardless of sucess processing the update.
+	-- Prevents showing API token on log since nginx error_log is not customizable.
+	-- Prevents trying to process troublesome updates more than once.
+	ngx.timer.at(0, process_update, update)
 
-	-- Parse the update
-	local response_body = main.parseMessageFunction(update)
-
-	-- Finally, encode the response and send it to Telegram
-	response_body = json.encode(response_body or {})
 	ngx.status = ngx.HTTP_OK
-	ngx.say(response_body)
-	ngx.log(ngx.DEBUG, "Outgoing response: "..response_body)
+	ngx.say("{}")
 	return ngx.exit(ngx.HTTP_OK)
 end
 
