db122b39e428ed94003c8c6c1535ecce39afe203
Yan Minari <yangm97@gmail.com>
2018-07-19 16:35:46 -0300

    Prevent dogpile effect on requests
    Signed-off-by: Yan Minari <yangm97@gmail.com>

--- lua/groupbutler/controller.lua
+++ lua/groupbutler/controller.lua
@@ -3,10 +3,10 @@ local _M = {}
 local json = require "cjson"
 local main = require "groupbutler.main"
 
--- local function process_update(_, update)
--- 	local bot = main.new(update)
--- 	bot:parseMessageFunction()
--- end
+local function process_update(update)
+	local bot = main.new(update)
+	bot:parseMessageFunction()
+end
 
 function _M.run()
 	local ok, update
@@ -20,17 +20,10 @@ function _M.run()
 		return ngx.exit(ngx.HTTP_BAD_REQUEST)
 	end
 
-	-- Process update on another thread. Returns HTTP_OK instantly, regardless of sucess processing the update.
-	-- Prevents showing API token on log since nginx error_log is not customizable.
-	-- Prevents trying to process troublesome updates more than once.
-	-- ngx.timer.at(0, process_update, update)
+	pcall(process_update, update)
 
 	ngx.status = ngx.HTTP_OK
 	ngx.say("{}")
-	ngx.eof()
-
-	local bot = main.new(update)
-	bot:parseMessageFunction()
 
 	return ngx.exit(ngx.HTTP_OK)
 end
