aed26303b52d314d024a0a9871b702951a4449af
Yan Minari <yangm97@gmail.com>
2018-08-28 17:20:52 -0300

    log bad updates
    Signed-off-by: Yan Minari <yangm97@gmail.com>

--- lua/groupbutler/controller.lua
+++ lua/groupbutler/controller.lua
@@ -2,25 +2,29 @@ local _M = {}
 
 local json = require "cjson"
 local main = require "groupbutler.main"
+local log = require "groupbutler.logging"
 
-local function process_update(_, update)
+local function process_update(_, update, update_json)
 	local update_obj = main:new(update)
-	update_obj:process()
+	-- TODO: make :process() return true on good updates
+	local ok --[[, retval]] = pcall(function() return update_obj:process() end)
+	if not ok --[[or not retval]] then
+		log.critical("Error processing update: {update}", {update = update_json})
+	end
 end
 
 function _M.run()
-	local ok, update
 	ngx.req.read_body()
-	update = ngx.req.get_body_data()
-	ngx.log(ngx.DEBUG, "Incoming update:", update)
+	local update_json = ngx.req.get_body_data()
+	ngx.log(ngx.DEBUG, "Incoming update:", update_json)
 
-	ok, update = pcall(json.decode, update)
+	local ok, update = pcall(json.decode, update_json)
 	if not ok then
 		ngx.status = ngx.HTTP_BAD_REQUEST
 		return ngx.exit(ngx.HTTP_BAD_REQUEST)
 	end
 
-	ngx.timer.at(0, process_update, update)
+	ngx.timer.at(0, process_update, update, update_json)
 
 	ngx.status = ngx.HTTP_OK
 	ngx.say("{}")
