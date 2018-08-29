c526ff1c4d28e4743268336d3fc980908b828d23
Yan Minari <yangm97@gmail.com>
2018-08-29 03:38:22 -0300

    Fix set_webhook endpoint
    Signed-off-by: Yan Minari <yangm97@gmail.com>

--- lua/init_nginx.lua
+++ lua/init_nginx.lua
@@ -1,9 +1,10 @@
 local config = require "groupbutler.config"
-local api = require "telegram-bot-api.methods".init(config.telegram.token)
+local methods = require "telegram-bot-api.methods"
 
 local _M = {}
 
 function _M.set_webhook()
+	local api = methods:new(config.telegram.token)
 	if not config.telegram.webhook.url and not config.telegram.webhook.domain then
 		ngx.log(ngx.WARN, "No webhook config detected. Check your config or set the webhook by yourself.")
 		return
@@ -17,13 +18,13 @@ function _M.set_webhook()
 	if config.telegram.webhook.url then
 		ngx.log(ngx.NOTICE, "Using manual webhook setup. Token check will be disabled.")
 		body.url = config.telegram.webhook.url
-		api.set_webhook(body)
+		api:set_webhook(body)
 		return
 	end
 
 	ngx.log(ngx.NOTICE, "Using express webhook setup.")
 	body.url = "https://"..config.telegram.webhook.domain.."/?token="..config.telegram.token
-	api.set_webhook(body)
+	api:set_webhook(body)
 	return
 end
 
