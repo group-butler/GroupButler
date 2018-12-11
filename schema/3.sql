cd7446bfb768e1a2048cba5e98a86556cfd280c0
Yan Minari <yangm97@gmail.com>
2018-12-11 06:31:25 -0200

    Update schema
    Signed-off-by: Yan Minari <yangm97@gmail.com>

--- schema/3.sql
+++ schema/3.sql
@@ -12,6 +12,7 @@ CREATE TABLE "chat_user" (
     chat_id bigint NOT NULL REFERENCES "chat"(id),
     user_id integer NOT NULL REFERENCES "user"(id),
     status chat_user_status NOT NULL DEFAULT 'member',
+    until_date timestamptz, -- Restricted and kicked only. Date when restrictions will be lifted for this user, if ever
 
     created_at timestamptz DEFAULT now() NOT NULL,
     updated_at timestamptz DEFAULT now() NOT NULL,
@@ -19,31 +20,30 @@ CREATE TABLE "chat_user" (
     CONSTRAINT chat_user_pkey PRIMARY KEY (chat_id, user_id)
 );
 
--- CREATE TABLE "chat_admin" (
---     chat_id bigint NOT NULL REFERENCES "chat"(id),
---     user_id integer NOT NULL REFERENCES "user"(id),
+CREATE TABLE "chat_admin" (
+    chat_id bigint NOT NULL REFERENCES "chat"(id),
+    user_id integer NOT NULL REFERENCES "user"(id),
 
---     can_be_edited boolean DEFAULT false NOT NULL,
---     can_change_info boolean DEFAULT false NOT NULL,
---     can_delete_messages boolean DEFAULT false NOT NULL,
---     can_invite_users boolean DEFAULT false NOT NULL,
---     can_restrict_members boolean DEFAULT false NOT NULL,
---     can_pin_messages boolean DEFAULT false NOT NULL,
---     can_promote_members boolean DEFAULT false NOT NULL,
---     -- can_post_messages boolean DEFAULT false NOT NULL, -- Channels only
---     -- can_edit_messages boolean DEFAULT false NOT NULL, -- Channels only
+    can_be_edited boolean DEFAULT false NOT NULL,
+    can_change_info boolean DEFAULT false NOT NULL,
+    can_delete_messages boolean DEFAULT false NOT NULL,
+    can_invite_users boolean DEFAULT false NOT NULL,
+    can_restrict_members boolean DEFAULT false NOT NULL,
+    can_pin_messages boolean DEFAULT false NOT NULL,
+    can_promote_members boolean DEFAULT false NOT NULL,
+    -- can_post_messages boolean DEFAULT false NOT NULL, -- Channels only
+    -- can_edit_messages boolean DEFAULT false NOT NULL, -- Channels only
 
---     created_at timestamptz DEFAULT now() NOT NULL,
---     updated_at timestamptz DEFAULT now() NOT NULL,
+    created_at timestamptz DEFAULT now() NOT NULL,
+    updated_at timestamptz DEFAULT now() NOT NULL,
 
---     CONSTRAINT chat_admin_pkey PRIMARY KEY (chat_id, user_id)
--- );
+    CONSTRAINT chat_admin_pkey PRIMARY KEY (chat_id, user_id)
+);
 
 -- CREATE TABLE "chat_restricted_user" (
 --     chat_id bigint NOT NULL REFERENCES "chat"(id),
 --     user_id integer NOT NULL REFERENCES "user"(id),
 
---     until_date timestamptz, -- Date when restrictions will be lifted for this user, if ever
 --     can_send_messages boolean DEFAULT true NOT NULL,
 --     can_send_media_messages boolean DEFAULT true NOT NULL, -- implies can_send_messages
 --     can_send_other_messages boolean DEFAULT true NOT NULL, -- implies can_send_media_messages
@@ -65,14 +65,23 @@ CREATE TRIGGER set_updated_at
     FOR EACH ROW
     EXECUTE PROCEDURE trigger_set_updated_at();
 
+CREATE TRIGGER set_updated_at
+    BEFORE UPDATE ON "chat_admin"
+    FOR EACH ROW
+    EXECUTE PROCEDURE trigger_set_updated_at();
+
 --
 -- Foreign Keys
 --
 
 ALTER TABLE chat_user
     ADD CONSTRAINT chat_user_chat_id_fkey FOREIGN KEY (chat_id)
-    REFERENCES "chat"(id) ON UPDATE CASCADE ON DELETE CASCADE;
+    REFERENCES "chat"(id) ON DELETE CASCADE;
 
 ALTER TABLE chat_user
     ADD CONSTRAINT chat_user_user_id_fkey FOREIGN KEY (user_id)
-    REFERENCES "user"(id) ON UPDATE CASCADE ON DELETE CASCADE;
+    REFERENCES "user"(id) ON DELETE CASCADE;
+
+ALTER TABLE chat_admin
+    ADD CONSTRAINT chat_admin_id_fkey FOREIGN KEY (chat_id, user_id)
+    REFERENCES "chat_user"(chat_id, user_id) ON DELETE CASCADE;
