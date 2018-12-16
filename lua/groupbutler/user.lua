fb677bf41ecc8ec33e7a99ead2c4511e0d581fa3
Yan Minari <yangm97@gmail.com>
2018-12-16 21:44:48 -0200

    Make it possible to fetch Chat and User properties using the dot syntax
    Signed-off-by: Yan Minari <yangm97@gmail.com>

--- lua/groupbutler/user.lua
+++ lua/groupbutler/user.lua
@@ -1,3 +1,5 @@
+local log = require("groupbutler.logging")
+
 local User = {}
 
 local function p(self)
@@ -5,16 +7,60 @@ local function p(self)
 end
 
 function User:new(obj, private)
+	assert(obj.id or obj.username, "User: Missing obj.id or obj.username")
+	assert(private.api, "User: Missing private.api")
 	assert(private.db, "User: Missing private.db")
 	setmetatable(obj, {
-		__index = self,
+		__index = function(s, index)
+			if self[index] then
+				return self[index]
+			end
+			return s:getProperty(index)
+		end,
 		__private = private,
 	})
+	if not obj:checkId() then
+		return nil, "Username not found"
+	end
 	return obj
 end
 
+function User:checkId()
+	local id = rawget(self, "id")
+	if not id then
+		id = p(self).db:getUserId(self.username)
+		if not id then
+			return false
+		end
+		self[id] = id
+	end
+	return true
+end
+
+function User:getProperty(index)
+	local property = rawget(self, index)
+	if property == nil then
+		property = p(self).db:getUserProperty(self, index)
+		if property == nil then
+			local ok = p(self).api:getChat(self.id)
+			if not ok then
+				log.warn("User: Failed to get {property} for {id}", {
+					property = index,
+					id = self.id,
+				})
+				return nil
+			end
+			self = ok
+			self:cache()
+			property = rawget(self, index)
+		end
+		self[index] = property
+	end
+	return property
+end
+
 function User:cache()
-	p(self).db:cache_user(self)
+	p(self).db:cacheUser(self)
 end
 
 return User
